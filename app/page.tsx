fad9bc8d576d48ba8cf199f3100322bef932430d
carnageitself <yashvharale@gmail.com>
2025-02-20 20:43:55 -0500

    fix: page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,10 @@ import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
 import { Experience } from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { Techstack } from "@/components/TechStack";
-import { ContainerScroll } from "@/components/ui/ContainerScroll";
 import { Wobble } from "@/components/Wobble";
-
-import { Bento } from "@/components/Grid";
-import { Glowing } from "@/components/Glowing";
 import HeroParallax from "@/components/ui/Parallax";
 
 const Home = () => {
