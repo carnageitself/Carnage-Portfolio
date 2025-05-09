0889d3ee6caa9124451c57c7e96e11cbca91972b
carnageitself <yashvharale@gmail.com>
2025-05-08 20:12:37 -0400

    refactor: data/index.ts (Tools added)

--- components/ui/Tooltip.tsx
+++ components/ui/Tooltip.tsx
@@ -15,7 +15,6 @@ export const AnimatedTooltip = ({
   items: {
     id: number;
     name: string;
-    designation: string;
     image: string;
   }[];
 }) => {
@@ -73,7 +72,6 @@ export const AnimatedTooltip = ({
                 <div className="font-bold text-white relative z-30 text-base">
                   {item.name}
                 </div>
-                
               </motion.div>
             )}
           </AnimatePresence>
@@ -83,7 +81,7 @@ export const AnimatedTooltip = ({
             width={100}
             src={item.image}
             alt={item.name}
-            className="object-cover !m-0 !p-0 object-top rounded-full h-10 w-10 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
+            className="object-contain bg-black  !m-0 !p-1 object-top rounded-full h-10 w-10 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
           />
         </div>
       ))}
--- data/index.ts
+++ data/index.ts
@@ -23,41 +23,275 @@ export const navItems = [
   { name: "Contact", link: "#contact" },
 ];
 
-const icons = [
+const EmployeeAttritionAnalysisIcons = [
   {
     id: 1,
-    name: "Tailwind CSS",
-    designation: "Software Engineer",
+    name: "Python",
+    image: "/python.png",
+  },
+  {
+    id: 2,
+    name: "NumPy",
+    image: "/numpy.png",
+  },
+  {
+    id: 3,
+    name: "Pandas",
+    image: "/pandas.svg",
+  },
+  {
+    id: 4,
+    name: "Power BI",
+    image: "/powerbi.png",
+  },
+  {
+    id: 6,
+    name: "Matplolib",
+    image: "/matplotlib.png",
+  },
+  {
+    id: 7,
+    name: "Seaborn",
+    image: "/seaborn.svg",
+  },
+  {
+    id: 8,
+    name: "Logistic Regression",
+    image: "/logistic-regression.png",
+  },
+  {
+    id: 9,
+    name: "Decision Tree",
+    image: "/decision-tree.png",
+  },
+  {
+    id: 10,
+    name: "Random Forest",
+    image: "/random forest.png",
+  },
+];
+
+const MotorVehicleCollisionsAnalysisIcons = [
+  {
+    id: 1,
+    name: "Python",
     image: "/tail.svg",
   },
   {
     id: 2,
-    name: "Typescript",
-    designation: "Product Manager",
+    name: "NumPy",
     image: "/ts.svg",
   },
   {
     id: 3,
-    name: "Next JS",
-    designation: "Data Scientist",
+    name: "Pandas",
     image: "/next.svg",
   },
   {
     id: 4,
-    name: "Three JS",
-    designation: "UX Designer",
+    name: "Power BI",
     image: "/three.svg",
   },
   {
+    id: 6,
+    name: "Matplolib",
+    image: "/gsap.svg",
+  },
+  {
+    id: 7,
+    name: "Seaborn",
+    image: "/gsap.svg",
+  },
+  {
+    id: 8,
+    name: "Logistic Regression",
+    image: "/gsap.svg",
+  },
+  {
+    id: 9,
+    name: "Decision Tree",
+    image: "/gsap.svg",
+  },
+  {
+    id: 10,
+    name: "Random Forest",
+    image: "/gsap.svg",
+  },
+];
+
+const AtliqSalesInsightsIcons = [
+  {
+    id: 1,
+    name: "MySQL",
+    image: "/mysql.png",
+  },
+  {
+    id: 2,
+    name: "Power BI",
+    image: "/powerbi.png",
+  },
+  {
+    id: 3,
+    name: "ETL Pipelines",
+    image: "/etl.png",
+  },
+  {
+    id: 4,
+    name: "Power Automate",
+    image: "/power-automate.png",
+  },
+  {
     id: 5,
-    name: "React JS",
-    designation: "Soap Developer",
-    image: "/re.svg",
+    name: "Python",
+    image: "/python.png",
+  },
+  {
+    id: 7,
+    name: "Matplolib",
+    image: "/matplotlib.png",
+  },
+  {
+    id: 8,
+    name: "Seaborn",
+    image: "/seaborn.svg",
+  },
+];
+
+const CarResalePredictionIcons = [
+  {
+    id: 1,
+    name: "Python",
+    image: "/python.png",
+  },
+  {
+    id: 2,
+    name: "NumPy",
+    image: "/numpy.png",
+  },
+  {
+    id: 3,
+    name: "Pandas",
+    image: "/pandas.svg",
   },
   {
     id: 6,
-    name: "GSAP",
-    designation: "The Explorer",
+    name: "Matplolib",
+    image: "/matplotlib.png",
+  },
+  {
+    id: 7,
+    name: "Seaborn",
+    image: "/seaborn.svg",
+  },
+  {
+    id: 8,
+    name: "Logistic Regression",
+    image: "/logistic-regression.png",
+  },
+  {
+    id: 9,
+    name: "Decision Tree",
+    image: "/decision-tree.png",
+  },
+  {
+    id: 10,
+    name: "Random Forest",
+    image: "/random forest.png",
+  },
+];
+
+const VehicleFuelEconomyAnalysisIcons = [
+  {
+    id: 1,
+    name: "Python",
+    image: "/tail.svg",
+  },
+  {
+    id: 2,
+    name: "NumPy",
+    image: "/ts.svg",
+  },
+  {
+    id: 3,
+    name: "Pandas",
+    image: "/next.svg",
+  },
+  {
+    id: 4,
+    name: "Power BI",
+    image: "/three.svg",
+  },
+  {
+    id: 6,
+    name: "Matplolib",
+    image: "/gsap.svg",
+  },
+  {
+    id: 7,
+    name: "Seaborn",
+    image: "/gsap.svg",
+  },
+  {
+    id: 8,
+    name: "Logistic Regression",
+    image: "/gsap.svg",
+  },
+  {
+    id: 9,
+    name: "Decision Tree",
+    image: "/gsap.svg",
+  },
+  {
+    id: 10,
+    name: "Random Forest",
+    image: "/gsap.svg",
+  },
+];
+
+const CrimeDataAnalysisIcons = [
+  {
+    id: 1,
+    name: "Python",
+    image: "/tail.svg",
+  },
+  {
+    id: 2,
+    name: "NumPy",
+    image: "/ts.svg",
+  },
+  {
+    id: 3,
+    name: "Pandas",
+    image: "/next.svg",
+  },
+  {
+    id: 4,
+    name: "Power BI",
+    image: "/three.svg",
+  },
+  {
+    id: 6,
+    name: "Matplolib",
+    image: "/gsap.svg",
+  },
+  {
+    id: 7,
+    name: "Seaborn",
+    image: "/gsap.svg",
+  },
+  {
+    id: 8,
+    name: "Logistic Regression",
+    image: "/gsap.svg",
+  },
+  {
+    id: 9,
+    name: "Decision Tree",
+    image: "/gsap.svg",
+  },
+  {
+    id: 10,
+    name: "Random Forest",
     image: "/gsap.svg",
   },
 ];
@@ -131,8 +365,8 @@ export const projects = [
     id: 1,
     title: "Employee Attrition Analysis",
     des: "Developed a machine learning pipeline to analyze and predict employee attrition using Python, with tools including Pandas,NumPy, Matplotlib, Seaborn, and Scikit-learn",
-    img: "/p1.svg",
-    iconLists: icons,
+    img: "/Employee Attrition Analysis.png",
+    iconLists: EmployeeAttritionAnalysisIcons,
     link: "https://github.com/carnageitself/Employee-Attrition-Analysis.git",
     github: "https://github.com/carnageitself/Employee-Attrition-Analysis.git",
   },
@@ -140,8 +374,8 @@ export const projects = [
     id: 2,
     title: "Atliq Sales Insights",
     des: "Automated data integration between MySQL and Power BI to enable real-time, dynamic reporting and eliminate the need for manual data uploads.",
-    img: "/p2.svg",
-    iconLists: icons,
+    img: "/Atliq.png",
+    iconLists: AtliqSalesInsightsIcons,
     link: "https://github.com/carnageitself/Atliq-Sales-Insights.git",
     github: "https://github.com/carnageitself/Atliq-Sales-Insights.git",
   },
@@ -149,40 +383,40 @@ export const projects = [
     id: 3,
     title: "Car Resale Prediction",
     des: "Created a predictive model to estimate car resale prices to help stakeholders in the automotive industry make informed decisions about inventory, purchases, and pricing strategies",
-    img: "/p3.svg",
-    iconLists: icons,
+    img: "/Car.png",
+    iconLists: CarResalePredictionIcons,
     link: "https://github.com/carnageitself/Car-Resale-Prediction.git",
     github: "https://github.com/carnageitself/Car-Resale-Prediction.git",
   },
-  {
-    id: 4,
-    title: "Motor Vehicle Collisions Analysis",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: icons,
-    link: "https://github.com/carnageitself/Motor-Vehicle-Collisions-Analysis.git",
-    github:
-      "https://github.com/carnageitself/Motor-Vehicle-Collisions-Analysis.git",
-  },
-  {
-    id: 5,
-    title: "Vehicle Fuel Economy Analysis and Market Trends",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: icons,
-    link: "https://github.com/carnageitself/Vehicle-Fuel-Economy-Analysis-and-Electric-Vehicle-Market-Trends.git",
-    github:
-      "https://github.com/carnageitself/Vehicle-Fuel-Economy-Analysis-and-Electric-Vehicle-Market-Trends.git",
-  },
-  {
-    id: 6,
-    title: "Crime Data Analysis",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: icons,
-    link: "https://github.com/carnageitself/Crime-Data-Analysis.git",
-    github: "https://github.com/carnageitself/Crime-Data-Analysis.git",
-  },
+  // {
+  //   id: 4,
+  //   title: "Motor Vehicle Collisions Analysis",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: MotorVehicleCollisionsAnalysisIcons,
+  //   link: "https://github.com/carnageitself/Motor-Vehicle-Collisions-Analysis.git",
+  //   github:
+  //     "https://github.com/carnageitself/Motor-Vehicle-Collisions-Analysis.git",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Vehicle Fuel Economy Analysis and Market Trends",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: VehicleFuelEconomyAnalysisIcons,
+  //   link: "https://github.com/carnageitself/Vehicle-Fuel-Economy-Analysis-and-Electric-Vehicle-Market-Trends.git",
+  //   github:
+  //     "https://github.com/carnageitself/Vehicle-Fuel-Economy-Analysis-and-Electric-Vehicle-Market-Trends.git",
+  // },
+  // {
+  //   id: 6,
+  //   title: "Crime Data Analysis",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: CrimeDataAnalysisIcons,
+  //   link: "https://github.com/carnageitself/Crime-Data-Analysis.git",
+  //   github: "https://github.com/carnageitself/Crime-Data-Analysis.git",
+  // },
 ];
 
 export const testimonials = [
